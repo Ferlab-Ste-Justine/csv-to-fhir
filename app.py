9835e0dbdf3319043108931da89e6a1e422554a4
jecos <jcostanza@ferlab.bio>
2021-11-08 20:33:18 -0500

    Override config with env var

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
-import yaml
-
 import oauth
+import config
 from fhir import build_bundle, bundle_entry, send_bundle
 from parse import parse_row
 from spreadsheet import load
 
 
-def load_config(f):
-    with open(f, "r") as stream:
-        return yaml.safe_load(stream)
-
-
 def main():
-    config = load_config('config.yml')
+    conf = config.load('config.yml')
     bundle_entries = []
-    for tab in config['file']['tabs']:
-        rows = load(config['file']['spreadsheetId'], config['file']['credentialFile'], tab['name'], tab.get('range'))
+    for tab in conf['file']['tabs']:
+        rows = load(conf['file']['spreadsheetId'], conf['file']['credentialFile'], tab['name'], tab.get('range'))
         headers = rows.pop(0)
         for row in rows:
             definition = {header: row[idx] if len(row) >= idx + 1 else None for idx, header in enumerate(headers)}
@@ -24,11 +18,11 @@ def main():
             bundle_entries.append(bundle_entry(parse_row(definition)))
     bundle = build_bundle(bundle_entries)
     access_token = None
-    if config['fhir'].get('oauth'):
-        access_token = oauth.get_access_token(config['fhir']['oauth']['url'], config['fhir']['oauth']['client_id'],
-                                              config['fhir']['oauth']['client_secret'])
+    if conf['fhir'].get('oauth'):
+        access_token = oauth.get_access_token(conf['fhir']['oauth']['url'], conf['fhir']['oauth']['client_id'],
+                                              conf['fhir']['oauth']['client_secret'])
     print(access_token)
-    send_bundle(config['fhir']['url'], bundle, access_token)
+    send_bundle(conf['fhir']['url'], bundle, access_token)
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,14 @@
+import yaml
+import os
+
+from deepmerge import always_merger
+
+from parse import parse_row
+
+
+def load(f):
+    with open(f, "r") as stream:
+        from_file = yaml.safe_load(stream)
+        from_environ = {k.removeprefix('config.'): v for k, v in os.environ.items() if k.startswith('config.')}
+        from_environ = parse_row(from_environ)
+        return always_merger.merge(from_file, from_environ)
