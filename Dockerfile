32c35691be02ed7788d9e3990f55a85938a56589
jecos <jcostanza@ferlab.bio>
2021-11-29 17:35:01 -0500

    Load multiple files

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,8 @@ COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
 COPY /app ./app
-COPY examples/config.yml .
+COPY examples/part1.yml .
+COPY examples/part2.yml .
 
 ENTRYPOINT [ "python3", "/app/app/main.py"]
-CMD ["-f", "config.yml"]
+CMD ["-f", "part1.yml", "-f", "part2.yml"]
--- app/main.py
+++ app/main.py
@@ -6,30 +6,31 @@ from app.spreadsheet import load
 import argparse
 
 
-def main(file):
-    print('Import started')
-    conf = config.load(file)
-    bundle_entries = []
-    for tab in conf['file']['tabs']:
-        rows = load(conf['file']['spreadsheetId'], conf['file']['credentialFile'], tab['name'], tab.get('range'))
-        headers = rows.pop(0)
-        for row in rows:
-            definition = {header: row[idx] if len(row) >= idx + 1 else None for idx, header in enumerate(headers)}
-            definition['resourceType'] = tab['resourceType']
-            definition['meta'] = {'profile': [tab['profile']]}
-            bundle_entries.append(bundle_entry(parse_row(definition)))
-    bundle = build_bundle(bundle_entries)
-    access_token = None
-    if conf['fhir'].get('oauth'):
-        access_token = oauth.get_access_token(conf['fhir']['oauth']['url'], conf['fhir']['oauth']['client_id'],
-                                              conf['fhir']['oauth']['client_secret'],
-                                              conf['fhir']['oauth'].get('uma_audience', None))
-    print('Sending bundle')
-    send_bundle(conf['fhir']['url'], bundle, access_token)
-    print('Import finished')
+def main(files):
+    for file in files:
+        print(f'Import {file} started')
+        conf = config.load(file)
+        bundle_entries = []
+        for tab in conf['file']['tabs']:
+            rows = load(conf['file']['spreadsheetId'], conf['file']['credentialFile'], tab['name'], tab.get('range'))
+            headers = rows.pop(0)
+            for row in rows:
+                definition = {header: row[idx] if len(row) >= idx + 1 else None for idx, header in enumerate(headers)}
+                definition['resourceType'] = tab['resourceType']
+                definition['meta'] = {'profile': [tab['profile']]}
+                bundle_entries.append(bundle_entry(parse_row(definition)))
+        bundle = build_bundle(bundle_entries)
+        access_token = None
+        if conf['fhir'].get('oauth'):
+            access_token = oauth.get_access_token(conf['fhir']['oauth']['url'], conf['fhir']['oauth']['client_id'],
+                                                  conf['fhir']['oauth']['client_secret'],
+                                                  conf['fhir']['oauth'].get('uma_audience', None))
+        print('Sending bundle')
+        send_bundle(conf['fhir']['url'], bundle, access_token)
+        print(f'Import {file} finished')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', help='Configuration file')
+    parser.add_argument('-f', '--file',action='append', help='Configuration file')
     args = parser.parse_args()
     main(args.file)
